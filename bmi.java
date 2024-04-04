0a4467731842d492722f5b65a60bab16b02dfba9
Yusuf Tufan <yusufftufan@gamil.com>
2024-04-05 00:20:01 +0300

    Güncellendi.

--- bmi.java
+++ bmi.java
@@ -1,17 +1,17 @@
-import java.util.Scanner;
+import java.util.Scanner; //Scanner tanımlamak.
 public class bmi {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Lutfen kilonuzu giriniz.");
-        double kilonuz = scanner.nextDouble();
+        double kilo = scanner.nextDouble();            //boy bilgisini almak.
         System.out.print("Lutfen boyunuzu giriniz.");
-        double boyunuz = scanner.nextDouble();
+        double boy = scanner.nextDouble();             //kilo bilgisini almak.
 
-        double Bmi = kilonuz/((boyunuz/100)*(boyunuz/100));
+        double Bmi = kilo/((boy/100)*(boy/100));       //vücut kitle endeksini hesaplamak.
 
-        String Bmi_aralik;
-        if(Bmi<18.5){
+        String Bmi_aralik;                            //Vücut kitle endeksi aralığı için değişken tanımlamak.
+        if(Bmi<18.5){                                 //if bloğunun tamamında ise aralık tespiti yapılıyor.
             Bmi_aralik ="Zayif";
         }else if(Bmi >=18.5 && Bmi<25){
             Bmi_aralik = "Normal";
@@ -20,8 +20,8 @@ public class bmi {
         }else{
             Bmi_aralik ="Obez";
         }
-        System.out.println("Vucut kitle Endeksi sonucunuz:"+Bmi);
-        System.out.println("BMI Kategoriniz:"+Bmi_aralik);
+        System.out.println("Vucut kitle Endeksi sonucunuz:"+Bmi);     //vücut kitle endeksini yazdır.
+        System.out.println("BMI Kategoriniz:"+Bmi_aralik);            //Vücut kitle endeksi aralığı yazdır.
         scanner.close();
     }
 }
